f806879322e86302ac62ec7c69f06ab918c68cc3
Sandro Costa e Silva <sandro.bcs@hotmail.com>
2025-09-19 00:36:31 -0300

    adição do construtor e dos getters

--- Participante.java
+++ Participante.java
@@ -1,4 +1,18 @@
 public class Participante {
     private String nomeCompleto;
     private String cpf;
+
+    public Participante(String nomeCompleto, String cpf) {
+        this.nomeCompleto = nomeCompleto;
+        this.cpf = cpf;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
 }
