f501f83232411ec4da47c0c742508f0c260ebb79
Arthur Dias da silva <arthurdiasdasilva@Arthurs-MacBook-Pro.local>
2025-09-26 18:03:08 -0300

    adição parametros e construtuor

--- Ingresso.java
+++ Ingresso.java
@@ -1,3 +1,12 @@
 public class Ingresso {
-    
+    private String codigoIngresso;     // Ex: 121-001 ou 121-075E
+    private String tipo;               // "Normal" ou "Especial"
+    private Participante participante; 
+
+    public Ingresso(String codigoIngresso, String tipo, Participante participante) {
+        this.codigoIngresso = codigoIngresso;
+        this.tipo = tipo;
+        this.participante = participante;
+    }
+
 }
